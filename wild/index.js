138d89234d8f4027ce082a809c01e07f3b439ddd
ted <ted@warpedpuppy.com>
2024-08-12 15:44:35 -0400

    convert radians to degrees

--- wild/index.js
+++ wild/index.js
@@ -8,6 +8,45 @@
   let dotWidth = 5;
   this.ctx.lineCap = "round";
 
+  // this.ctx.beginPath();
+  // this.ctx.arc(halfWidth, halfHeight, 200, 0, 2 * Math.PI);
+  // this.ctx.moveTo(0, halfHeight)
+  // this.ctx.lineTo(this.canvasWidth, halfHeight)
+  // this.ctx.moveTo(halfWidth, 0)
+  // this.ctx.lineTo(halfWidth, this.canvasHeight)
+  // this.ctx.stroke();
+
+  function start() {
+    let length = 200;
+    this.ctx.beginPath();
+    this.ctx.moveTo(halfWidth, halfHeight);
+    this.ctx.lineTo(halfWidth + length, halfHeight)
+    this.ctx.arc(halfWidth + length, halfHeight, 100, 0, 2 * Math.PI);
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(halfWidth, halfHeight);
+    this.ctx.lineTo(halfWidth - length, halfHeight);
+    this.ctx.arc(halfWidth - length, halfHeight, 100, 0, 2 * Math.PI);
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(halfWidth, halfHeight);
+    this.ctx.lineTo(halfWidth, halfHeight + length);
+    this.ctx.stroke();
+    this.ctx.beginPath();
+    this.ctx.arc(halfWidth, halfHeight + length, 100, 0, 2 * Math.PI);
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(halfWidth, halfHeight);
+    this.ctx.lineTo(halfWidth, halfHeight - length);
+    this.ctx.stroke();
+    this.ctx.beginPath();
+    this.ctx.arc(halfWidth, halfHeight - length, 100, 0, 2 * Math.PI);
+    this.ctx.stroke();
+  }
+
   this.ctx.beginPath();
   this.ctx.arc(halfWidth, halfHeight, 200, 0, 2 * Math.PI);
   this.ctx.moveTo(0, halfHeight)
@@ -16,29 +55,34 @@
   this.ctx.lineTo(halfWidth, this.canvasHeight)
   this.ctx.stroke();
 
-  getSixty();
-  function getSixty() {
-    let radius = 200;
+  getSixty(200, { x: halfWidth, y: halfHeight });
+
+
+
+  function getSixty(radius, centerPoint) {
     let newX = (1 / 2) * radius;
     let finalY = (Math.sqrt(3) / 2) * radius;
 
     this.ctx.strokeStyle = "green";
     this.ctx.lineWidth = 3;
     this.ctx.beginPath();
-    this.ctx.moveTo(halfWidth, halfHeight);
-    this.ctx.lineTo(halfWidth + newX, halfHeight)
-    this.ctx.lineTo(halfWidth + newX, halfHeight - finalY)
-    this.ctx.lineTo(halfWidth, halfHeight);
+    this.ctx.moveTo(centerPoint.x, centerPoint.y);
+    this.ctx.lineTo(centerPoint.x + newX, centerPoint.y)
+    this.ctx.lineTo(centerPoint.x + newX, centerPoint.y - finalY)
+    this.ctx.lineTo(centerPoint.x, centerPoint.y);
     this.ctx.stroke();
 
     this.ctx.beginPath();
-    this.ctx.moveTo(halfWidth, halfHeight);
-    this.ctx.lineTo(halfWidth - newX, halfHeight)
-    this.ctx.lineTo(halfWidth - newX, halfHeight - finalY)
-    this.ctx.lineTo(halfWidth, halfHeight);
+    this.ctx.moveTo(centerPoint.x, centerPoint.y);
+    this.ctx.lineTo(centerPoint.x - newX, centerPoint.y)
+    this.ctx.lineTo(centerPoint.x - newX, centerPoint.y - finalY)
+    this.ctx.lineTo(centerPoint.x, centerPoint.y);
     this.ctx.stroke();
 
+    return { x: centerPoint.x + newX, y: centerPoint.y - finalY }
+
   }
+  getThirty();
   function getThirty() {
     let radius = 200;
     let newX = (Math.sqrt(3) / 2) * radius;
@@ -61,12 +105,13 @@
     this.ctx.stroke();
   }
   thirtyWithSinAndCos()
-  getThirty()
   function thirtyWithSinAndCos() {
     let radius = 200;
-    let newX = Math.cos(60) * radius;
-    let finalY = Math.sin(60) * radius;
-
+    let newX = (Math.cos(60) * (180 / Math.pi)) * radius;
+    let finalY = (Math.sin(60) * (180 / Math.pi)) * radius;
+    console.log("thirty:", Math.sin(30), Math.cos(30))
+    console.log("sixty:", Math.sin(60), Math.cos(60))
+    console.log(Math.cos(30), (Math.sqrt(3) / 2) * (Math.PI / 180))
     this.ctx.strokeStyle = "green";
     this.ctx.lineWidth = 3;
     this.ctx.beginPath();
@@ -83,28 +128,30 @@
     this.ctx.lineTo(halfWidth, halfHeight);
     this.ctx.stroke();
   }
+  sixtyWithSinAndCos()
+  function sixtyWithSinAndCos() {
+    let radius = 200;
+    let newX = (Math.sin(60) * (180 / Math.pi)) * radius;
+    let finalY = (Math.cos(60) * (180 / Math.pi)) * radius;
 
+    this.ctx.strokeStyle = "green";
+    this.ctx.lineWidth = 3;
+    this.ctx.lineCap = "round";
+    this.ctx.beginPath();
+    this.ctx.moveTo(halfWidth, halfHeight);
+    this.ctx.lineTo(halfWidth + newX, halfHeight)
+    this.ctx.lineTo(halfWidth + newX, halfHeight + finalY)
+    this.ctx.lineTo(halfWidth, halfHeight);
+    this.ctx.stroke();
 
-  let radius = 200;
-  let newX = Math.sin(60) * radius;
-  let finalY = Math.cos(60) * radius;
+    this.ctx.beginPath();
+    this.ctx.moveTo(halfWidth, halfHeight);
+    this.ctx.lineTo(halfWidth - newX, halfHeight)
+    this.ctx.lineTo(halfWidth - newX, halfHeight + finalY)
+    this.ctx.lineTo(halfWidth, halfHeight);
+    this.ctx.stroke();
 
-  this.ctx.strokeStyle = "green";
-  this.ctx.lineWidth = 3;
-  this.ctx.lineCap = "round";
-  this.ctx.beginPath();
-  this.ctx.moveTo(halfWidth, halfHeight);
-  this.ctx.lineTo(halfWidth + newX, halfHeight)
-  this.ctx.lineTo(halfWidth + newX, halfHeight + finalY)
-  this.ctx.lineTo(halfWidth, halfHeight);
-  this.ctx.stroke();
-
-  this.ctx.beginPath();
-  this.ctx.moveTo(halfWidth, halfHeight);
-  this.ctx.lineTo(halfWidth - newX, halfHeight)
-  this.ctx.lineTo(halfWidth - newX, halfHeight + finalY)
-  this.ctx.lineTo(halfWidth, halfHeight);
-  this.ctx.stroke();
+  }
 
 
 })()
